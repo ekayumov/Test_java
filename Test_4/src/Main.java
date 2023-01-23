e682e7209950a977c33c060a5792300549e26ee5
ekayumov <107910871+ekayumov@users.noreply.github.com>
2023-01-23 17:14:24 +0300

    fix

--- Test_4/src/Main.java
+++ Test_4/src/Main.java
@@ -2,24 +2,36 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         String filePath = args[0];
         ArrayList<String> lines = (ArrayList<String>) Files.readAllLines(Paths.get(filePath));
         int sum = 0;
         int[] nums = new int[lines.size()];
+        int[] sum_num = new int[lines.size()];
 
         for (int i = 0; i < lines.size(); i++) {
             nums[i] = Integer.parseInt(lines.get(i));
-            sum += nums[i];
         }
-        int sumSteps = 0;
-        int centre = (int) Math.ceil((float)sum/lines.size());
 
         for (int i = 0; i < lines.size(); i++) {
-            sumSteps += Math.abs (centre - nums[i]);
+            sum = 0;
+            for (int j = 0; j< lines.size(); j++) {
+                sum += Math.abs (nums[i] - nums[j]);
+            }
+            sum_num[i] = sum;
         }
-        System.out.println(sumSteps);
+
+        List<Integer> ints = Arrays.stream(sum_num)
+                .boxed()
+                .collect(Collectors.toList());
+
+        System.out.println( Collections.min(ints));
 
     }
 }
